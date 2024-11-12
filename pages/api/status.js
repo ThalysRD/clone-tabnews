3809d915b915dbf160315b3db7cf711f8b0bb6b5
Thalys Rodrigues <tgbr66@gmail.com>
2024-11-12 14:34:09 -0300

    Cria o endpoint /status

---
+++ pages/api/status.js
@@ -0,0 +1,5 @@
+function status(req, res) {
+  res.status(200).json({ Chave: "Que massinha" });
+}
+
+export default status;
--- tests/unit/ calculadora.test.js
+++ tests/unit/ calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("Somar 2 + 2 deve ser igual a 4", () => {
   expect(calculadora.somar(2, 2)).toBe(4);
