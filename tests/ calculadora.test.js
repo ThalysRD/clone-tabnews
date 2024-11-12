7e972077349b381814c21e2cff23037334ed2a84
Thalys Rodrigues <tgbr66@gmail.com>
2024-11-12 09:34:12 -0300

    Cria testes automatizados

---
+++ tests/ calculadora.test.js
@@ -0,0 +1,13 @@
+const calculadora = require("../models/calculadora.js");
+
+test("Somar 2 + 2 deve ser igual a 4", () => {
+  expect(calculadora.somar(2, 2)).toBe(4);
+});
+
+test("Somar 5 + 100 deve ser igual a 105", () => {
+  expect(calculadora.somar(5, 100)).toBe(105);
+});
+
+test("Somar 'Banana' + 100 deveria retornar 'ERRO'", () => {
+  expect(calculadora.somar("Banana", 100)).toBe("ERRO");
+});
